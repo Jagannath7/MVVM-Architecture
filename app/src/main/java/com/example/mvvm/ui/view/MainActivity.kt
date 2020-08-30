fad94f656ec8200e0835eb730d0d9995e6ed0730
Jayanti Jagannath <jagannath.jyanti5@gmail.com>
2020-08-30 18:19:38 +0530

    project structure updated

---
+++ app/src/main/java/com/example/mvvm/ui/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mvvm.ui.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mvvm.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
